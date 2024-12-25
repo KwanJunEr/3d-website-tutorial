14f21429cbe9255c0773177fc91831163fb595ce
KwanJunEr <jonastheitguy@gmail.com>
2024-12-25 23:08:59 +0800

    ccompleted faq section

--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Element } from "react-scroll";
 import { faq } from "../constants";
+import FaqItem from "../components/FaqItem";
 
 const Faq = () => {
 
@@ -27,14 +28,24 @@ const halfLength = Math.floor(faq.length/2);
                 </div>
 
                 <div className="relative flex-1 pt-24">
-                    {faq.slice(0, halfLength).map((faq, index)=>(
+                    {faq.slice(0, halfLength).map((item, index)=>(
                         <div>
-                            {faq.question}
+                            <FaqItem key={item.id} item= {item} index = {index}/>
+                        </div>
+                    ))}
+                </div>
+
+                <div className="relative flex-1 lg:pt-24">
+                    {faq.slice(halfLength).map((item, index)=>(
+                        <div>
+                            <FaqItem key={item.id} item= {item} index = {halfLength + index}/>
                         </div>
                     ))}
                 </div>
 
             </div>
+
+            <div className="faq-line_after absolute left-[calc(50% -1px)] top-0 -z-1 h-full w-0.5 bg-s2 max-lg:hidden" />
         </div>
       </Element>
     </section>
